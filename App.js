6aaa78e966a40ba3af14adf42b05ab799515fa2d
Phatwasin Suksai <phatwasin01@gmail.com>
2023-10-01 19:36:59 +0700

    lab6.2

--- App.js
+++ App.js
@@ -1,158 +1,148 @@
 import React, { Component } from "react";
-
 import {
   Platform,
-  StyleSheet,
   Text,
   View,
-  TouchableOpacity,
+  StyleSheet,
   Dimensions,
-  Switch,
+  TouchableHighlight,
+  Button,
 } from "react-native";
+import Constants from "expo-constants";
+import * as Location from "expo-location";
 
-import { LineChart } from "react-native-chart-kit";
+import MapView, { Marker } from "react-native-maps";
+var { width, height } = Dimensions.get("window");
 
-import StockButton from "./StockButton.js";
-
-import API from "./api.js";
-
-const chartConfig = {
-  backgroundGradientFrom: "#1E2923",
-  backgroundGradientTo: "#08130D",
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`, // color of background
-  strokeWidth: 2, // optional, default 3
-};
-
-export default class Stocks extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.changeIndex = this.changeIndex.bind(this);
     this.state = {
-      dates: ["01/01", "02/01", "03/01", "04/01", "05/01", "06/01", "07/01"],
-      prices: [1, 2, 3, 4, 5, 6, 7],
-      stockname: "Choose a stock",
-      switch: false,
+      location: null,
+      errorMessage: null,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      pin: false,
     };
-
-    // this.data = {
-    //   labels: this.state.dates,
-    //   datasets: [{
-    //     data: this.state.prices,
-    //     color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // colar of the line
-    //     strokeWidth: 2 // optional
-    //   }]
-    // }
+    this.onRegionChangeComplete = this.onRegionChangeComplete.bind(this);
+    this.handleDropPin = this.handleDropPin.bind(this);
   }
 
-  changeIndex(stockCode, stockName) {
-    console.log(stockCode, stockName);
-    API(stockCode, this.state.switch).then((stock) => {
-      let keyTimeSeries = this.state.switch
-        ? "Weekly Time Series"
-        : "Time Series (Daily)";
-      // console.log(stock);
-      let datesArray = Object.keys(stock[keyTimeSeries]).slice(0, 6);
-      let closingPrice = [];
-      datesArray.forEach((day) => {
-        closingPrice.push(stock[keyTimeSeries][day]["4. close"]);
-      });
-      let datesArrayRev = datesArray.reverse();
-      let datesNotincludeYear = datesArrayRev.map((date) => {
-        let [year, month, day] = date.split("-");
-        return `${day}/${month}`;
-      });
-      console.log(datesArray);
-      console.log(closingPrice);
-      console.log(datesArrayRev);
+  onRegionChangeComplete(region) {
+    this.setState({ region });
+  }
+
+  handleDropPin() {
+    console.log("pin drop");
+    this.setState({ pin: true });
+  }
+
+  componentDidMount() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
       this.setState({
-        dates: datesNotincludeYear,
-        prices: closingPrice,
-        stockname: stockName,
+        errorMessage:
+          "Oops, this will not work on Sketch in an Android emulator. Try it on your device!",
       });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied",
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    // console.log(location);
+    const region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
+    // console.log(region);
+    this.setState({
+      region: region,
+      location: location,
     });
+  };
+
+  getJSXshow(error) {
+    if (error)
+      return <Text style={styles.paragraph}>{this.state.errorMessage}</Text>;
+    else {
+      return (
+        <View>
+          <MapView
+            style={styles.map}
+            region={this.state.region}
+            onRegionChangeComplete={this.onRegionChangeComplete}
+          >
+            {this.state.pin ? (
+              <Marker
+                key={"key"}
+                coordinate={{
+                  latitude: this.state.region.latitude,
+                  longitude: this.state.region.longitude,
+                }}
+                title={"my location"}
+                description={
+                  String(this.state.region.latitude) +
+                  ", " +
+                  String(this.state.region.longitude)
+                }
+                fixed
+              />
+            ) : null}
+          </MapView>
+          <View>
+            <Button title="Drop Pin" onPress={this.handleDropPin} />
+          </View>
+        </View>
+      );
+    }
   }
 
   render() {
-    let message;
-    if (this.state.switch) {
-      message = "Weekly";
-    } else {
-      message = "Daily";
+    // let text = 'Waiting..';
+    let error;
+    if (this.state.errorMessage) {
+      error = true;
+    } else if (this.state.location) {
+      error = false;
     }
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text>{this.state.stockname}</Text>
-          <LineChart
-            data={{
-              labels: this.state.dates,
-              datasets: [
-                {
-                  data: this.state.prices,
-                  color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // colar of the line
-                  strokeWidth: 2, // optional
-                },
-              ],
-            }}
-            width={Dimensions.get("window").width}
-            height={220}
-            chartConfig={chartConfig}
-            style={{ paddingVertical: 10 }}
-          />
-        </View>
-        <View style={styles.footer}>
-          <StockButton code="AAPL" name="Apple" onPress={this.changeIndex} />
-          <StockButton code="GOOGL" name="Google" onPress={this.changeIndex} />
-          <StockButton code="UBER" name="Uber" onPress={this.changeIndex} />
-        </View>
-        <View style={styles.switchContainer}>
-          <Text style={{ fontSize: 30 }}>{message}</Text>
-          <Switch
-            onValueChange={() => {
-              this.setState({
-                switch: !this.state.switch,
-              });
-            }}
-            value={this.state.switch}
-          />
-        </View>
-      </View>
-    );
+
+    return <View style={styles.container}>{this.getJSXshow(error)}</View>;
   }
 }
 
 const styles = StyleSheet.create({
-  switchContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
   container: {
     flex: 1,
-  },
-  header: {
-    flex: 2,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
+    justifyContent: "flex-start",
+    backgroundColor: "#ecf0f1",
   },
-  footer: {
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    backgroundColor: "pink",
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: "center",
+  },
+  map: {
+    width: width,
+    height: Math.floor((height * 80) / 100),
   },
   button: {
-    margin: 10,
-    borderWidth: 1,
-    width: 100,
-    height: 50,
-    borderRadius: 10,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "lightgray",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
   },
 });
